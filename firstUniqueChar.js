6d695abbfc2e43ace9b3bc5c847b6c4de101564f
Lekshmi <sreelekshmi310192@gmail.com>
2025-06-26 18:25:07 +0530

    first unique character in a string

---
+++ firstUniqueChar.js
@@ -0,0 +1,35 @@
+/* Q. 387
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+*/
+
+var firstUniqChar = function(s) {
+const charCount = new Map();
+
+  for (let char of s) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (charCount.get(s[i]) === 1) {
+      return i;
+    }
+  }
+
+  return -1;    
+    
+};
